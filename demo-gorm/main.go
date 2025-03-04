2ab17f9ec0eac1a659ebe5bdf340441536f4b2e0
李勇 <2236472089@qq.com>
2025-03-04 23:11:48 +0800

    go

--- demo-gorm/main.go
+++ demo-gorm/main.go
@@ -9,12 +9,16 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		// users := []models.User{} // 使用复数命名更清晰
-		users := models.User{} // 使用复数命名更清晰
+	r.GET("/list", func(c *gin.Context) {
+		users := []models.User{} // 使用复数命名更清晰
+		// users := models.User{} // 使用复数命名更清晰
+
+		// result := models.DB.Where("id>1 AND id<4").Find(&users) // 条件
+
+		result := models.DB.Where("age LIKE?", "%22%").Find(&users) // 条件
 
 		// 传入指针，并处理错误
-		result := models.DB.Find(&users)
+		// result := models.DB.Find(&users)
 		if result.Error != nil {
 			c.JSON(500, gin.H{"error": result.Error.Error()})
 			return
